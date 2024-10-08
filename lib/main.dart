dc11015c01bf07bdd8c443d9e48003d772cf7a64
2001Kaye <jhan117@naver.com>
2024-10-08 19:49:09 +0900

    🐛 버그 수정: 최적화 및 BuildContext 이슈 해결
    - const 키워드를 추가하여 코드 최적화
    - 생성자에 key를 추가
    - 불필요한 파일 삭제
    - BuildContext 비동기 갭 이슈로 화면 클릭 시 메인으로 전환하도록 수정
    - 텍스트 노란줄 경고 문제를 DefaultTextStyle로 해결

--- lib/main.dart
+++ lib/main.dart
@@ -10,34 +10,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SplashScreenPage(), // 스플래시 화면 표시
-    );
-  }
-}
-
-class SplashScreenPage extends StatefulWidget {
-  @override
-  _SplashScreenPageState createState() => _SplashScreenPageState();
-}
-
-class _SplashScreenPageState extends State<SplashScreenPage> {
-  @override
-  void initState() {
-    super.initState();
-    // 3초 후에 메인 화면으로 전환
-    Future.delayed(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const MainApp()), // 메인 화면으로 전환
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SplashScreen(), // 피그마에서 가져온 스플래시 화면을 표시
+    return const MaterialApp(
+      home: SplashScreen(), // 스플래시 화면 표시
     );
   }
 }
--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,43 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:graytalk/main.dart';
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
     final screenHeight = MediaQuery.of(context).size.height;
 
-    return Container(
-      width: screenWidth,
-      height: screenHeight,
-      clipBehavior: Clip.antiAlias,
-      decoration: const BoxDecoration(
-        color: Colors.white,
+    return GestureDetector(
+      onTap: () => Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const MainApp()), // 메인 화면으로 전환
       ),
-      child: Stack(
-        children: [
-          Positioned(
-            left: 0,
-            top: 0,
-            child: Container(
-              width: screenWidth,
-              height: screenHeight,
-              decoration: const BoxDecoration(
-                color: Color(0xFFF8F3E6),
-              ),
-              child: Center(
-                child: Text(
-                  'Graytalk',
-                  style: TextStyle(
-                    color: Color(0xFFF1B2B2),
-                    fontSize: 27,
-                    fontFamily: 'Sarpanch',
-                    fontWeight: FontWeight.w900,
+      child: Container(
+        width: screenWidth,
+        height: screenHeight,
+        clipBehavior: Clip.antiAlias,
+        decoration: const BoxDecoration(
+          color: Colors.white,
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              left: 0,
+              top: 0,
+              child: Container(
+                width: screenWidth,
+                height: screenHeight,
+                decoration: const BoxDecoration(
+                  color: Color(0xFFF8F3E6),
+                ),
+                child: const Center(
+                  child: DefaultTextStyle(
+                    style: TextStyle(
+                      color: Color(0xFFF1B2B2),
+                      fontSize: 27,
+                      fontFamily: 'Sarpanch',
+                      fontWeight: FontWeight.w900,
+                    ),
+                    child: Text('Graytalk'),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
