b5a9f8fef1003dd8e266e439c80e628e38053d69
2001Kaye <jhan117@naver.com>
2024-10-09 21:23:55 +0900

    ✨ 기능 추가: 스플래시 스크린의 인트로 스크린 구현

---
+++ lib/core/utils/intro_messages.dart
@@ -0,0 +1,19 @@
+class IntroMessages {
+  static const messages = [
+    {
+      "img": 'assets/imgs/intro/stone_friend.png',
+      "title": "친구 만들기",
+      "description": "당신만의 돌멩이 친구를 만들어보세요. 작은 돌에서 특별한 친구가 탄생합니다!"
+    },
+    {
+      "img": 'assets/imgs/intro/chat.png',
+      "title": "대화하기",
+      "description": "돌멩이 친구와의 대화를 시작해보세요. 작은 돌에서 큰 위로를 얻을 수 있습니다."
+    },
+    {
+      "img": 'assets/imgs/intro/mood_light.png',
+      "title": "무드등 켜기",
+      "description": "따뜻한 빛으로 공간을 밝혀줄 무드등을 켜보세요. 돌멩이 친구와 함께하는 편안한 시간!"
+    },
+  ];
+}
---
+++ lib/presentation/widgets/intro_header.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:graytalk/presentation/pages/root_screen.dart';
+
+class IntroHeader extends StatelessWidget {
+  final int pageNum;
+  final int pageLength;
+
+  const IntroHeader({
+    super.key,
+    required this.pageNum,
+    required this.pageLength,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        TextButton(
+          onPressed: null,
+          child: Text(
+            '${pageNum.toString()}/$pageLength',
+          ),
+        ),
+        TextButton(
+          onPressed: () {
+            Navigator.of(context).pushReplacement(
+                MaterialPageRoute(builder: (context) => const RootScreen()));
+          },
+          child: const Text(
+            'Skip',
+          ),
+        ),
+      ],
+    );
+  }
+}
