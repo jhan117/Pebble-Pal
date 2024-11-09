88024b817c76ff9adb49039b04973500ddc2add7
Kaye <jhan117@naver.com>
2024-11-09 22:25:49 +0900

    ♻️ 코드 리팩토링: PageProvider 삭제 및 QuestionProvider 코드 개선 후 question box의 클릭 로직 수정

--- lib/presentation/widgets/question_box.dart
+++ lib/presentation/widgets/question_box.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:graytalk/core/theme/colors.dart';
+import 'package:graytalk/presentation/pages/write_screen.dart';
 
 class QuestionBox extends StatelessWidget {
   final String questionText;
@@ -16,43 +17,47 @@ class QuestionBox extends StatelessWidget {
     final textTheme = Theme.of(context).textTheme;
     List<String> splitText = questionText.split('\n');
 
-    return Stack(
-      children: [
-        Container(
-          width: double.infinity,
-          height: 140,
-          padding: const EdgeInsets.only(top: 32, bottom: 32),
-          margin: const EdgeInsets.only(left: 32, right: 32),
-          decoration: BoxDecoration(
-            color: colorScheme.surfaceContainer,
-            borderRadius: BorderRadius.circular(12),
+    return GestureDetector(
+      onTap: () => Navigator.of(context).pushReplacement(
+          MaterialPageRoute(builder: (context) => const WriteScreen())),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 140,
+            padding: const EdgeInsets.only(top: 32, bottom: 32),
+            margin: const EdgeInsets.only(left: 32, right: 32),
+            decoration: BoxDecoration(
+              color: colorScheme.surfaceContainer,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  splitText[0],
+                  style: textTheme.bodyMedium,
+                ),
+                splitText.length > 1
+                    ? Text(
+                        splitText[1],
+                        style: textTheme.bodyMedium,
+                      )
+                    : const SizedBox.shrink(),
+              ],
+            ),
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                splitText[0],
-                style: textTheme.bodyMedium,
-              ),
-              splitText.length > 1
-                  ? Text(
-                      splitText[1],
-                      style: textTheme.bodyMedium,
-                    )
-                  : const SizedBox.shrink(),
-            ],
+          Positioned(
+            top: 4,
+            right: 36,
+            child: IconButton(
+              icon: const Icon(Icons.refresh),
+              iconSize: 32,
+              onPressed: onRefresh,
+            ),
           ),
-        ),
-        Positioned(
-          top: 4,
-          right: 36,
-          child: IconButton(
-            icon: const Icon(Icons.refresh),
-            iconSize: 32,
-            onPressed: onRefresh,
-          ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
